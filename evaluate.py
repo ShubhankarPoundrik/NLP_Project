0b9e1e95b37214774641a1c72421bf4a63ec7ccc
Yalagach <nakshatra1998@gmail.com>
2023-12-12 00:13:14 -0500

    Added evaluation

--- evaluate.py
+++
@@ -1,8 +0,0 @@
-
-
-
-
-
-if __name__ == "__main__":
-    str = "The dog is located at the middleRight of the image and the person is located at the middleCenter of the image."
-    sentence = str.split()
